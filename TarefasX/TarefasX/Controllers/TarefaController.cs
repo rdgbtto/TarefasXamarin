7b4ee19bc9dff9c8f49e2495e4d072c5842759da
Rodrigo Almeida Bonotto <rdgbtto@gmail.com>
2019-09-15 13:26:08 -0300

    Creating TarefaController.

---
+++ TarefasX/TarefasX/Controllers/TarefaController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using TarefasX.Models;
+
+namespace TarefasX.Controllers
+{
+    public class TarefaController
+    {
+        private List<Tarefa> Lista { get; set; }
+
+        public void Salvar(Tarefa tarefa)
+        {
+            Lista.Add(tarefa);
+            AtualizarLista(Lista);
+        }
+
+        public void Finalizar(int index, Tarefa tarefa)
+        {
+            Lista.RemoveAt(index);
+            Lista.Add(tarefa);
+            AtualizarLista(Lista);
+        }
+
+        public void Deletar(Tarefa tarefa)
+        {
+            Lista.Remove(tarefa);
+            AtualizarLista(Lista);
+        }
+
+        public List<Tarefa> ListarTarefas()
+        {
+            return BuscarLista();
+        }
+
+        private void AtualizarLista(List<Tarefa> lista)
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                App.Current.Properties.Remove("Tarefas");
+            }
+            App.Current.Properties.Add("Tarefas", lista);
+        }
+
+        private List<Tarefa> BuscarLista()
+        {
+            return App.Current.Properties.ContainsKey("Tarefas") ? (List<Tarefa>)App.Current.Properties["Tarefas"] : new List<Tarefa>();
+        }
+    }
+}
---
+++ TarefasX/TarefasX/Models/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TarefasX.Models
+{
+    public class Tarefa
+    {
+        public string Nome { get; set; }
+        public DateTime DataFinalizacao { get; set; }
+        public byte Prioridade { get; set; }
+    }
+}
